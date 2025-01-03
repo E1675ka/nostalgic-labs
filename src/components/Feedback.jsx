d58575409c92feb62ac45069c680de3201c11781
E1675ka <jacobangel254@gmaiul.com>
2025-01-03 08:03:37 -0800

    service section

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -12,14 +12,14 @@ const FeedbackCard = ({
   testimonial,
  
 }) => (
-  <motion.
+  <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
     className="bg-black-200/50 p-10 rounded-3xl xs:w-[320px] w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
 
     <div className="mt-1">
-      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
+      <p className="text-white tracking-wider  text-[18px]">{testimonial}</p>
 
       {/* <div className="mt-7 flex justify-between items-center gap-1">
         <div className="flex-1 flex flex-col">
@@ -31,7 +31,7 @@ const FeedbackCard = ({
           </p>
       </div> */}
     </div>
-  </motion.>
+  </motion.div>
 );
 
 const Feedbacks = () => {
