260145271c3e725403eb3ad2d172513e088cf511
E1675ka <E1675ka>
2025-01-30 13:42:18 -0800

    changed conflicts in assets exports index js

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,4 +1,3 @@
-import nostalgicLogo from "./logo_nostalgic_1.webp";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import webgif from "./webgif.json";
@@ -10,6 +9,20 @@ import innovation from "./innovation.json";
 import transparency from "./transparency.json";
 import clienttrust from "./clienttrust.json";
 import clientSupport from "./client-support-anim.json";
-
-import Nlogo from "./NlLogo.png"
-export {Nlogo,mainScene,clienttrust,clientSupport,transparency,innovation,cta,sw,seo,graphic,webgif,github,redux,nostalgicLogo, menu, close,nodejs,reactjs,threejs,mongodb,javascript,git,tailwind };
+import mainScene from "./Main-Scene.json";
+import Nlogo from "./NlLogo.png";
+export {
+  Nlogo,
+  clienttrust,
+  clientSupport,
+  transparency,
+  innovation,
+  cta,
+  sw,
+  seo,
+  graphic,
+  webgif,
+  menu,
+  close,
+  mainScene,
+};
