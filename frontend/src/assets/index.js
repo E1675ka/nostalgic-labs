168249cf12a73960173cf9d85c8532e425a51925
E1675ka <E1675ka>
2025-01-30 13:22:53 -0800

    delleted unused icons

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,15 +1,6 @@
 import nostalgicLogo from "./logo_nostalgic_1.webp";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import nodejs from "./nodejs.png";
-import reactjs from "./reactjs.png";
-import threejs from "./threejs.svg";
-import mongodb from "./mongodb.png";
-import javascript from "./javascript.png";
-import git from "./git.png";
-import tailwind from "./tailwind.png";
-import redux from "./redux.png";
-import github from "./github.png";
 import webgif from "./webgif.json";
 import graphic from "./graphic.json";
 import seo from "./seo.json";
@@ -19,6 +10,6 @@ import innovation from "./innovation.json";
 import transparency from "./transparency.json";
 import clienttrust from "./clienttrust.json";
 import clientSupport from "./client-support-anim.json";
-import mainScene from "./main-Scene.json"
+
 import Nlogo from "./NlLogo.png"
 export {Nlogo,mainScene,clienttrust,clientSupport,transparency,innovation,cta,sw,seo,graphic,webgif,github,redux,nostalgicLogo, menu, close,nodejs,reactjs,threejs,mongodb,javascript,git,tailwind };
