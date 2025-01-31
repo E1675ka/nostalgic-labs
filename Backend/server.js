b4f23142f0b1dd9398af7c54360a50db14bafc69
E1675ka <E1675ka>
2025-01-31 03:43:35 -0800

    servetest

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,9 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({ origin: "*", methods: "GET,POST,PUT,DELETE", credentials: true })
+);
 
 // Middleware
 app.use(express.json()); 
