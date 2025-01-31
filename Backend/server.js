0844b88fc50fba9700279eb33522779428880eb9
E1675ka <E1675ka>
2025-01-31 03:33:08 -0800

    c

--- Backend/server.js
+++ Backend/server.js
@@ -29,6 +29,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  console.log(`Request Origin: ${req.headers.origin}`);
+  next();
+});
 
 console.log("MONGO_URI:", process.env.MONGO_URI);
 
