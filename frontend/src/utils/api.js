b79187d68d603dd53f275f2540977429c1033716
E1675ka <E1675ka>
2025-01-31 03:08:10 -0800

    changed the api file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,12 +3,4 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export const fetchData = async () => {
-  try {
-    const response = await axios.get(`${apiUrl}/api/some-endpoint`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-};
+export default apiUrl;
